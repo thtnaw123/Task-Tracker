c298f11ee39ad46427154ac345c6014e49f75ba3
Tihitinaw <Thtnawbuze@gmail.com>
2025-10-12 17:18:38 +0400

    Student B - Task model added

---
+++ TaskTracker/Models/TaskModel.swift
@@ -0,0 +1,15 @@
+//
+//  TaskModel.swift
+//  TaskTracker
+//
+//  Created by Tihitinaw Buzuwek on 12/10/2025.
+//
+
+import Foundation
+
+
+struct Task: Identifiable {
+    let id = UUID()
+    var title: String
+    var isCompleted: Bool
+}

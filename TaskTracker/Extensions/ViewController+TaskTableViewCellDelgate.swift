9fba2a33f075098f6521ee6245fd1a3a31153e44
Tihitinaw <Thtnawbuze@gmail.com>
2025-10-13 00:30:59 +0400

    Student A - variable renamed in view cell delgate

--- TaskTracker/Extensions/ViewController+TaskTableViewCellDelgate.swift
+++ TaskTracker/Extensions/ViewController+TaskTableViewCellDelgate.swift
@@ -10,8 +10,8 @@ import Foundation
 extension ViewController: TaskTableViewCellDelgate {
     func changeTaskStatusHandler(taskIndex: Int) {
         let targetTask = taskViewModel.getAllTasks()[taskIndex]
-        if let tasksVar = taskViewModel.toggleTaskStatus(taskId: targetTask.id) {
-            taskList = tasksVar
+        if let tasksVariable = taskViewModel.toggleTaskStatus(taskId: targetTask.id) {
+            taskList = tasksVariable
         }
         taskListTableView.reloadData()
     }
